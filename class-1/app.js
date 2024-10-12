9ca65721fccffaba33233c600f8fcdb639c11047
Qadir39 <sa9183238@gmail.com>
2024-10-12 18:58:44 +0500

    add flies and folders

---
+++ class-1/app.js
@@ -0,0 +1,81 @@
+// alert("hello world");
+// document.write("this is hello world");
+// console.log("tis is also again hello world");
+
+// var a = 5;
+// var b = 10;
+// var c = 20;
+// alert(c);
+
+// var firstName = "Ali";
+// var lastname = "Raza";
+
+// document.write(firstName +" "+    lastname);
+
+// var firstName = "bilal";
+// var lastName = "Ahmed";
+
+// alert(firstName +" "+ lastName);
+
+// var num1 = 20;
+// var num2 = 30;
+// var num3 = 50 + num2;
+// console.log(num1+num2 +num3;)
+
+
+
+
+// ****************post icrement and decrement
+// var num1 = 20;
+// var num2 = 30;
+// var num3 = 50;
+// console.log(num1+num2 +num3++);
+// console.log(num1+num2 +num3++);
+// console.log(num1+num2 +num3--);
+// console.log(num1+num2 +num3--);
+
+
+// console.log(num1+num2 +num3++);
+// console.log(num1+num2 +num3--);
+// console.log(num1+num2 +num3--);
+
+//  ***************pre increment and decrement
+
+// var num1 = 20;
+// var num2 = 30;
+// var num3 = 50;
+// console.log(num1+num2 +--num3);
+// console.log(num1+num2 + ++num3);
+
+
+//************************ BODMAS
+
+// var calculation =((20+40)*10)/20;
+// console.log(calculation);
+
+// var calculation =((200/40)+10)*20;
+// console.log(calculation);
+
+
+//************* IF STATEMENT*********************
+
+// var inp = prompt("enter your city");
+
+// if (inp=="Karachi") {
+// alert("It is a wonderful city ");
+
+// }
+// else{
+//     alert("welcome to"+" "+inp);
+// }
+
+// var user = prompt("enter your age");
+
+// if (user>20) { 
+//     alert("you are adult ");
+// }
+// else{
+//     alert("you are not adult");
+// }
+
+
---
+++ class-2/app.js
@@ -0,0 +1,79 @@
+// ********* === CONDEITION K SATH DATA TYPE CHECK KARTA HAI **************
+// var userCity = prompt("Where does the Qadir live ?");
+// if (userCity==="karachi") {
+//     alert("welcome qadir")
+//  }
+
+
+// ****************** NOT OPRETER ***********
+
+//  var userCity = prompt("Where does the Qadir live ?");
+// if (userCity!=="karachi") {
+//     alert("welcome qadir")
+//  }
+
+
+// *************** && OPRETER ******************
+
+// var userCity = prompt("ENTER CITY NAME");
+// var userAge = +prompt("enter your age");
+// var weight = +prompt("enter your weight");
+
+// if(userCity==="karachi" && userAge>=20 && weight>=50  ){
+//     alert("visit our gym");
+// }
+
+// else{
+//     alert("your not eligible");
+// }
+
+
+// ****************** || OR OPRETER **********
+
+// var userCity = prompt("ENTER CITY NAME");
+// var userAge = +prompt("enter your age");
+// var weight = +prompt("enter your weight");
+
+// if(userCity==="karachi" || userAge>=20 && weight>=50  ){
+//     alert("visit our gym");
+// }
+
+// else{
+//     alert("your not eligible");
+// }
+
+
+// **************** IF ELSE  AND ELSE IF **************
+
+// var userCity = prompt("Where does the Qadir live ?");
+// if (userCity==="karachi") {
+//     alert("welcome qadir")
+//  }
+//  else{
+//  (alert("invalid city!!"))
+//  }
+
+
+
+
+// ************* CAALCULATOR PROGRAM ***********
+
+// var num1 = +prompt("enter your first number");
+// var num2 = +prompt("enter your second number");
+// var sign = prompt("enter a sign");
+
+// if (sign === "+") {
+//     alert(num1 + num2);
+// }
+// else if (sign === "-") {
+//     alert(num1 - num2);
+// }
+// else if (sign === "*") {
+//     alert(num1 * num2);
+// }
+// else if (sign === "/") {
+//     alert(num1 / num2);
+// }
+// else if (sign === "%") {
+//     alert(num1 * num2);
+// }
